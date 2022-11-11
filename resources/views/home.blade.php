f6ab14c19ec9c051cf8ae218db46bcdbb26f805f
thiagop127001 <thiagogua2009@gmail.com>
2022-11-11 07:55:07 -0300

    primeiro commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>LocVel - Inicio</title>
+    @livewireStyles
+</head>
+<body>
+    <div>
+        <a href="#">Categorias</a>
+        <a href="">Marcas</a>
+        <a href="">Carros</a>
+    </div>
+    @livewireScripts
+</body>
+</html>
---
+++ resources/views/livewire/categories.blade.php
@@ -0,0 +1,6 @@
+<div>
+    
+    <h1 class="text-3xl font-bold underline">
+        Categories!
+      </h1>
+</div>
