6a012b870297e92e76dbc1006c6e08b35e6ce2b0
thiagop127001 <thiagogua2009@gmail.com>
2022-11-14 08:09:06 -0300

    criada função store

--- app/Http/Livewire/Categories.php
+++ app/Http/Livewire/Categories.php
@@ -3,11 +3,20 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Category;
 
 class Categories extends Component
 {
+    public $categoria;
+   
     public function render()
     {
         return view('livewire.categories');
+    }
+
+    public function store()
+    {
+        $name = $this->categoria;
+        // faltar rodar migration para salvar
     }
 }
