b842019bae78280345c9c32227de94117ce38caf
thiagop127001 <thiagogua2009@gmail.com>
2022-11-16 09:10:31 -0300

    criado layout navbar

--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -1,3 +1,43 @@
-<div class="">
-    <a href="/">Inicio</a>
+<div class="px-6 pt-6 ">
+    <div>
+        <nav class="flex h-9 items-center justify-between" aria-label="Global">
+            <div class="flex lg:min-w-0 " aria-label="Global">
+                <a href="/" class="-m-1.5 p-1.5">
+                    <span class="sr-only">Your Company</span>
+                    <img class="h-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                        alt="">
+                </a>
+            </div>
+            <div class="h-4 grid grid-cols-1 gap-y-3 md:hidden">
+                <a href="/categories" class="font-semibold text-gray-900 hover:text-gray-900">Categorias</a>
+
+                <a href="/cars" class="font-semibold text-gray-900 hover:text-gray-900">Carros</a>
+
+                <a href="#" class="font-semibold text-gray-900 hover:text-gray-900">Modelos</a>
+
+                <a href="#" class="font-semibold text-gray-900 hover:text-gray-900">Lojas</a>
+                <hr>
+                <a href="#"
+                    class="inline-block rounded-lg px-3 py-1.5 text-sm font-semibold leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20">Log
+                    in</a>
+            </div>
+
+            <div class="hidden md:flex md:min-w-0  md:justify-center md:gap-x-12">
+                <a href="/categories" class="font-semibold text-gray-900 hover:text-gray-900">Categorias</a>
+
+                <a href="/cars" class="font-semibold text-gray-900 hover:text-gray-900">Carros</a>
+
+                <a href="#" class="font-semibold text-gray-900 hover:text-gray-900">Modelos</a>
+
+                <a href="#" class="font-semibold text-gray-900 hover:text-gray-900">Lojas</a>
+
+            </div>
+            <div class="hidden md:flex md:min-w-0  md:justify-end">
+                <a href="#"
+                    class="inline-block rounded-lg px-3 py-1.5 text-sm font-semibold leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20">Log
+                    in</a>
+            </div>
+        </nav>
+    </div>
 </div>
+<br>
